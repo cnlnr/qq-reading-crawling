eeab90dfdff3ea9d9fab5f0da272d1c2b0b49a2a
LZY <2217445143@qq.com>
2025-07-24 17:18:30 +0800

    模拟点击登录按钮

--- 登录.py
+++ 登录.py
@@ -6,6 +6,6 @@ with sync_playwright() as p:
     context = browser.new_context()
     page = context.new_page()
     page.goto("https://book.qq.com/")
+    page.click('p.unlogin.ypc-link')
     input("登录完成后按回车继续...")
-    # 存储缓存数据
     context.storage_state(path="browser_cache.json")
