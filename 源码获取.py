595d93f90918a2d226b0244318a14c0cdc114c70
LZY <2217445143@qq.com>
2025-07-25 16:36:10 +0800

    只获取正文

--- 源码获取.py
+++ 源码获取.py
@@ -1,5 +1,6 @@
 import requests
 import json
+from selectolax.parser import HTMLParser
 
 url = "https://book.qq.com/book-read/34895175/51"
 
@@ -8,9 +9,14 @@ with open("browser_cache.json", "r", encoding="utf-8") as f:
     storage = json.load(f)
 cookies = {cookie['name']: cookie['value'] for cookie in storage['cookies']}
 
-# 发起请求
-response = requests.get(url, cookies=cookies)
+# 获取源码
+html = requests.get(url, cookies=cookies).text
 
-# 保存页面内容
+# 获取正文（保留HTML标签）
+parser = HTMLParser(html)
+div_article = parser.css_first('div#article')
+div_article_html = div_article.html if div_article else "<p>没有找到正文</p>"
+
+# 保存正文
 with open("index.html", "w", encoding="utf-8") as f:
-    f.write(response.text)
+    f.write(div_article_html)
