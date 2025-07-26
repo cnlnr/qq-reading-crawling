c80ff02b724351600b359818403627688a457335
LZY <2217445143@qq.com>
2025-07-26 15:47:10 +0800

    自动命名

--- 源码获取.py
+++ 源码获取.py
@@ -1,8 +1,22 @@
 import requests, json
 from selectolax.parser import HTMLParser
+from pathlib import Path
 
-cookies = {c['name']: c['value'] for c in json.load(open("cookies.json", encoding="utf-8"))['cookies']}
-html = requests.get("https://book.qq.com/book-read/34895175/51", cookies=cookies).text
-parser = HTMLParser(html).css_first('div#article')
-div_article = parser.html[85:-6] if parser else "<p>没有找到正文</p>"
-open("index.html", "w", encoding="utf-8").write(div_article)
+book_id = "34895175"
+parser = HTMLParser(requests.get(f"https://book.qq.com/book-read/{book_id}/51", cookies={c['name']: c['value'] for c in json.load(open("cookies.json", encoding="utf-8"))['cookies']}).text)
+
+# 获取书名并创建目录
+book_name = parser.css_first('a.book-title').text()
+book_path = Path("dataset", f"{book_name}_{book_id}")
+book_path.mkdir(parents=True, exist_ok=True)
+
+# 获取章节名
+chapter_name = parser.css_first('meta[name="keywords"]').attributes.get('content')[len(book_name)+1:]
+
+# 获取正文并保存
+open(book_path/f"{chapter_name}.html", "w", encoding="utf-8").write(parser.css_first('div#article').html[85:-6])
+
+# 检查是否全书完
+div_book_end = parser.css_first('div.book-end.ypc-column-name')
+if div_book_end:
+    print("全书完")
