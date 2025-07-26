22f461ba411db8d9340d3e5481152016145d0fb4
LZY <2217445143@qq.com>
2025-07-26 18:26:47 +0800

    优化+遍历章节完工

--- 章节获取.py
+++ 章节获取.py
@@ -3,13 +3,17 @@ from selectolax.parser import HTMLParser
 from pathlib import Path
 import json
 
-def crawl_book_chapter(cid=51, bid=34895175):
+def crawl_chapter(cid: int, bid: int) -> bool:
+    """获取QQ阅读章节内容
+    
+    Args:
+        cid: 章节ID
+        bid: 书籍ID
+        
+    Returns:
+        bool: False 返回最后章节
     """
-    根据提供的章节 ID 和书籍 ID 爬取书籍章节内容并保存为 HTML 文件。
 
-    :param cid: 章节 ID，默认为 51
-    :param bid: 书籍 ID，默认为 34895175
-    """
     parser = HTMLParser(requests.get(f"https://book.qq.com/book-read/{bid}/{cid}", cookies={c['name']: c['value'] for c in json.load(open("cookies.json", encoding="utf-8"))['cookies']}).text)
 
     # 获取书名并创建目录
@@ -21,8 +25,12 @@ def crawl_book_chapter(cid=51, bid=34895175):
     chapter_name = parser.css_first('meta[name="keywords"]').attributes.get('content')[len(book_name)+1:]
 
     # 获取正文并保存
-    open(book_path/f"{chapter_name}.html", "w", encoding="utf-8").write(parser.css_first('div#article').html[85:-6])
+    open(book_path/f"{cid}_{chapter_name}.html", "w", encoding="utf-8").write(parser.css_first('div#article').html[85:-6])
 
+    # 检查是否是最后一章
+    div_book_end = parser.css_first('div.book-end.ypc-column-name')
+    if div_book_end:
+        return False
 
 if __name__ == "__main__":
-    crawl_book_chapter()
+    crawl_chapter(60, 34895175)
--- 遍历章节.py
+++ 遍历章节.py
@@ -1,6 +1,14 @@
 import 章节获取
 
-# 检查是否全书完
-div_book_end = parser.css_first('div.book-end.ypc-column-name')
-if div_book_end:
-    print("全书完")
+chapter_id = 60
+book_id = 34895175
+
+while True:
+    is_finished = 章节获取.crawl_chapter(chapter_id, book_id)
+    
+    if is_finished == False:
+        print(f"爬取完成")
+        break
+    
+    print(f"第{chapter_id}章完成", end='\r')
+    chapter_id += 1
