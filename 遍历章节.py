dd562d2204f1db341ade890d2eb6d89638be05c2
LZY <2217445143@qq.com>
2025-07-27 14:37:14 +0800

    优化

--- 遍历章节.py
+++ 遍历章节.py
@@ -1,16 +1,19 @@
 import 章节获取
 
-def crawl_chapters(chapter_id, book_id):
 
+def crawl_chapters(book_id):
+    """遍历章节"""
+    chapter_id = 1
     while True:
         is_finished = 章节获取.crawl_chapter(chapter_id, book_id)
-        
+
         if is_finished == False:
-            print("爬取完成")
             break
-        
+
         print(f"第{chapter_id}章完成", end='\r')
         chapter_id += 1
 
+
 if __name__ == "__main__":
-    crawl_chapters(60, 34895175)
+    crawl_chapters(34895175)
+    print("遍历完成")
