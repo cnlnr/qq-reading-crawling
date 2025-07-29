efbcb77980fe6300963f2cc6ff653bc468ec6def
LZY <2217445143@qq.com>
2025-07-29 14:40:01 +0800

    优化

--- 登录.py
+++ 登录.py
@@ -1,10 +1,8 @@
 from playwright.sync_api import sync_playwright
 
 with sync_playwright() as p:
-    browser = p.firefox.launch(headless=False)
-    context = browser.new_context()
+    context = p.firefox.launch(headless=False).new_context()
     page = context.new_page()
-    page.goto("https://book.qq.com/")
-    page.click('p.unlogin.ypc-link')
+    page.goto("https://book.qq.com/user-center")
     input("登录完成后按回车继续...")
     context.storage_state(path="cookies.json")
