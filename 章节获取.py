482efc4cdfccbf3e54c2350d5d23441e2402ced4
LZY <2217445143@qq.com>
2025-08-07 13:32:52 +0800

    个性化

--- 章节获取.py
+++ 章节获取.py
@@ -2,24 +2,28 @@ import requests, json
 from pathlib import Path
 
 # 获取指定章节
-def read(bid, cid, cookies):
+def read\
+    (bid, cid, cookies):
     read_data = requests.get(f'https://ubook.reader.qq.com/api/book/read?bid={bid}&cid={cid}', cookies=cookies).json()['data']
     title = read_data.get('title')
     content = read_data.get('content')
     return title, content
 
 # 获取书名和总章节数
-def detail(bid, cookies):
+def detail\
+    (bid, cookies):
     book_detail = requests.get(f'https://ubook.reader.qq.com/api/book/detail?bid={bid}', cookies=cookies).json()['data']['book']
     book_title = book_detail['title']
     totalChapters = book_detail['totalChapters']
     return book_title, totalChapters
 
-def cookies(json_path):
+def cookies\
+    (json_path):
     return {c['name']: c['value'] for c in json.load(open(json_path))['cookies']}
 
 # 遍历章节
-def traverse_chapters(bid, cookies):
+def traverse_chapters\
+    (bid, cookies):
     # 获取书名和总章节数
     book_title, totalChapters = detail(bid, cookies)
     # 遍历章节
